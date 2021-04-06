3791889e7d457de273a98116e6b465f5bf509685
yaser <yaser.94ae@gmail.com>
2021-04-06 12:05:00 +0200

    about activiry

--- Servicios Plataformas Móviles/Android/Filmoteca/app/src/main/java/es/ua/eps/mm2021/kotlin/filmoteca/AboutActivity.kt
+++ Servicios Plataformas Móviles/Android/Filmoteca/app/src/main/java/es/ua/eps/mm2021/kotlin/filmoteca/AboutActivity.kt
@@ -5,7 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import es.ua.eps.mm2021.kotlin.filmoteca.R
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +17,10 @@ class AboutActivity : AppCompatActivity() {
         val webButton = findViewById<Button>(R.id.webButton)
         val supportButton = findViewById<Button>(R.id.supportButton)
         val backButton = findViewById<Button>(R.id.backButton)
+        val googleData = findViewById<TextView>(R.id.googleData)
+        val account = user.account
+
+        googleData.text = String.format(" Id: %s \nName %s \nEmail %s", account?.id, account?.givenName, account?.email)
 
         webButton.setOnClickListener {
             val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ulompi.cards"))

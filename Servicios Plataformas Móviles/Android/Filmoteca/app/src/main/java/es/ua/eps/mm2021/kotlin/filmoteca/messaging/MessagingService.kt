8f23cf1226c897385da7d221790b2dc8e0e5bc73
yaser <yaser.94ae@gmail.com>
2021-04-06 16:34:57 +0200

    notificación primer plano

--- Servicios Plataformas Móviles/Android/Filmoteca/app/src/main/java/es/ua/eps/mm2021/kotlin/filmoteca/messaging/MessagingService.kt
+++ Servicios Plataformas Móviles/Android/Filmoteca/app/src/main/java/es/ua/eps/mm2021/kotlin/filmoteca/messaging/MessagingService.kt
@@ -1,6 +1,20 @@
 package es.ua.eps.mm2021.kotlin.filmoteca.messaging
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Looper.prepare()
+
+        Handler().post {
+            Toast.makeText(baseContext, remoteMessage.notification?.title, Toast.LENGTH_LONG).show()
+        }
+        Looper.loop()
+    }
 }

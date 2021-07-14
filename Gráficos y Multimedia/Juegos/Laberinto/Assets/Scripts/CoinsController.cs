479acd5955833928e16d56a49f6eeeccec19396c
yasmanets <34303875+yasmanets@users.noreply.github.com>
2021-07-14 19:25:18 +0200

    sin vidas

--- Gráficos y Multimedia/Juegos/Laberinto/Assets/Scripts/CoinsController.cs
+++ Gráficos y Multimedia/Juegos/Laberinto/Assets/Scripts/CoinsController.cs
@@ -11,6 +11,7 @@ public class CoinsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CoinsController.coins = 0;
         actualizarCoins();
     }
 
--- Gráficos y Multimedia/Juegos/Laberinto/Assets/Scripts/LifeManagement.cs
+++ Gráficos y Multimedia/Juegos/Laberinto/Assets/Scripts/LifeManagement.cs
@@ -7,18 +7,28 @@ public class LifeManagement : MonoBehaviour
 {
     public Text heartText;
     public static int vidas = 0;
+    public AudioClip loseAudio;
+    public Text coinText;
+    public GameObject panel;
+    public GameObject exitButton;
+    public Text winText;
 
     // Start is called before the first frame update
     void Start()
     {
+        LifeManagement.vidas = 0;
         actualizarVidas();
+        winText.enabled = false;
+        exitButton.SetActive(false);
+        panel.SetActive(false);
     }
 
     public void actualizarVidas() {
         heartText.text = "Vidas: " + LifeManagement.vidas;
     }
 
-    public void sumarVida() {
+    public void sumarVida()
+    {
         if (LifeManagement.vidas < 3) 
         {
             LifeManagement.vidas++;
@@ -26,7 +36,20 @@ public class LifeManagement : MonoBehaviour
         }
     }
 
-    public void restarVida() {
+    public void restarVida()
+    {
+        if (LifeManagement.vidas == 0) 
+        {
+            AudioSource.PlayClipAtPoint(loseAudio, transform.position);
+            coinText.enabled = false;
+            heartText.enabled = false;
+            winText.enabled = true;
+            exitButton.SetActive(true);
+            panel.SetActive(true);
+            
+            winText.text = "No tienes vidas...";
+            Time.timeScale = 0f;
+        }
         LifeManagement.vidas--;
         actualizarVidas();
     }
